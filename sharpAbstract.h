e2894b9a22a8f2c676815e481e812e21b836c52f
Nata <proskurinanata1997@mail.ru>
2020-05-17 20:26:07 +0700

    Реализован абстрактный класс для C#

---
+++ sharpAbstract.h
@@ -0,0 +1,16 @@
+#ifndef SHARPABSTRACT_H
+#define SHARPABSTRACT_H
+#include "abstractMethod.h"
+#include "sharpCassUnit.h"
+#include "sharpmethodunit.h"
+#include "sharpprintunit.h"
+using Flags = unsigned int;
+
+class AbstractSharp : public AbstractMethod
+{
+public:
+    std::shared_ptr<AbstractClassUnit> createClassUnit(const std::string& name, Flags modifier = 0);
+    std::shared_ptr<AbstractMethodUnit> createMethodUnit(const std::string& name, const std::string& returnType, Flags flags);
+    std::shared_ptr<AbstractPrintUnit> createPrintOperatorUnit(const std::string& text);
+};
+#endif // SHARPABSTRACT_H
